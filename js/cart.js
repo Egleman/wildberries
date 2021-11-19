827b7427447d59537babf73ea6928d5ba4837a95
Egleman <34210494+Egleman@users.noreply.github.com>
2021-11-19 15:16:51 +0300

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,16 @@
+const cart = function(){
+    const cartBtn = document.querySelector('.button-cart');
+    const cart = document.querySelector(".overlay");
+    const closeBtn = document.querySelector('.modal-close');
 
+    cartBtn.addEventListener('click', function () {
+        cart.style.display = 'flex'
+    });
+
+    closeBtn.addEventListener('click', function () {
+        cart.style.display = ''
+    })
+
+}
+
+cart();
